6d3062ace508927331f21a70c4842127f7b3190e
slee <slee@magicsoft.co.kr>
2023-07-11 15:24:02 +0900

    빈 생성자 추가

--- src/main/java/com/blog/request/PostCreate.java
+++ src/main/java/com/blog/request/PostCreate.java
@@ -1,11 +1,12 @@
 package com.blog.request;
 
 import lombok.Getter;
-import lombok.Setter;
+import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotBlank;
 
 @Getter
+@NoArgsConstructor
 public class PostCreate {
     @NotBlank
     private String title;
